e53421ac7aa485d39a0553eea99e076415de3b7f
Karol Furmański <karolfur1995@gmail.com>
2018-11-25 15:40:30 +0100

    add front to rooms

--- Source/wwwroot/js/chat.js
+++ Source/wwwroot/js/chat.js
@@ -4,6 +4,22 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
 connection.on("HandleMessage", function (user, message) {
     console.log("User " + user + " said: " + message);
+
+    var div = document.createElement("div");
+    div.className = "message";
+    var text_user = document.createElement("p");
+    var text_message = document.createElement("p");
+    text_user.textContent = user;
+    text_message.textContent = message;
+    text_user.className = "message-text-user";
+    text_message.className = "message-text-message";
+    div.appendChild(text_user);
+    div.appendChild(text_message);
+    document.getElementById("messagesList").insertAdjacentElement('afterbegin', div);
+
+
+// To dostaje nadawca i odbiorca
+
     /*
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     //var encodedMsg = user + " says " + msg;
@@ -25,10 +41,34 @@ connection.on("HandleMessage", function (user, message) {
 
 connection.on("OnUserJoined", function (username) {
     console.log("User " + username + " has joined the chat");
+
+    var div = document.createElement("div");
+    div.className = "message_joined";
+    var text_user = document.createElement("p");
+    var text_message = document.createElement("p");
+    text_user.textContent = username;
+    text_message.textContent = "Has joined the chat";
+    text_user.className = "message-text-user";
+    text_message.className = "message-text-message";
+    div.appendChild(text_user);
+    div.appendChild(text_message);
+    document.getElementById("messagesList").insertAdjacentElement('afterbegin', div);
 });
 
 connection.on("OnUserLeft", function (username) {
     console.log("User " + username + " has left the chat");
+
+    var div = document.createElement("div");
+    div.className = "message_left";
+    var text_user = document.createElement("p");
+    var text_message = document.createElement("p");
+    text_user.textContent = username;
+    text_message.textContent = "Has left the chat";
+    text_user.className = "message-text-user";
+    text_message.className = "message-text-message";
+    div.appendChild(text_user);
+    div.appendChild(text_message);
+    document.getElementById("messagesList").insertAdjacentElement('afterbegin', div);
 });
 
 connection.start().catch(function (err) {
@@ -36,6 +76,10 @@ connection.start().catch(function (err) {
 });
 
 document.getElementById("send-message-button").addEventListener("click", function (event) {
+
+// To dostaje nadawca
+
+
     /*var user = document.getElementById("userInput").value;*/
     var message = document.getElementById("message-input").value;
     connection.invoke("SendMessage", message).catch(function (err) {
